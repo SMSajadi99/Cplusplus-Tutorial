834fc609c3cd54d2cb102e5ee23ddf015a138564
Seyed Mohammad Sajadi <s.m.sajadi.sut@gmail.com>
2023-11-16 17:36:44 +0330

    Lecture09 - Multi Input

---
+++ Lecture/Lecture09-MultiInput.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1, num2, num3;
+    cin >> num1 >> num2 >> num3;
+    cout << "The first number: " << num1 << "\nThe second number: " << num2 << "\nThe third number: " << num3;
+    return 0;
+}
