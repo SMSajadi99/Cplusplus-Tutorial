58d897729f1e3b786c3781e935972f67c25ceb6d
Seyed Mohammad Sajadi <s.m.sajadi.sut@gmail.com>
2023-11-16 19:43:21 +0330

    Lecture12 - Convert variables

---
+++ Lecture/Lecture12-ConvertVariables.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int d_num = 2;
+    float f_num = float(d_num);
+    cout << d_num << " ---- " << f_num << endl;
+    return 0;
+}
---
+++ Lecture/Lecture12-ConvertVariables02.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << float(140) << endl;
+    cout << int(140.0000000) << endl;
+    cout << int(140.5221354) << endl;
+    cout << int(140.9999999) << endl;
+    cout << int(-140.655) << endl;
+    cout << int(-140.355) << endl;
+    return 0;
+}
