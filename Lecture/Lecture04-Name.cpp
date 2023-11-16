35787e8074816d40fb2a40d29d29111414d6056a
Seyed Mohammad Sajadi <s.m.sajadi.sut@gmail.com>
2023-11-16 15:02:11 +0330

    Lecture04 - Name

---
+++ Lecture/Lecture04-Name.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string firstname = "Seyed Mohammad";
+    string lastname = " Sajadi";
+    cout << firstname + lastname;
+    
+    return 0;
+}
