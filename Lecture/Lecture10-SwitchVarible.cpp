99ab47202d2d0df724e838809a687f2dc85c2abc
Seyed Mohammad Sajadi <s.m.sajadi.sut@gmail.com>
2023-11-16 17:48:23 +0330

    Lecture10 - Switch varible

---
+++ Lecture/Lecture10-SwitchVarible.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a , b , c;
+    cin >> a >> b;
+
+    c = a;
+    a = b;
+    b = c;
+
+    cout << a << "\n" << b;
+    return 0;
+}
---
+++ Lecture/Lecture11-VariableMemory.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+
+    cout << "The int varaible takes: " << sizeof(num) << "bytes(blocks of memory)!" << endl;
+    return 0;
+}
