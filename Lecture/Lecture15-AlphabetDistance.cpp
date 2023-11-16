ee2c7e292e84986018fd4f1af47359ec3397e9d8
Seyed Mohammad Sajadi <s.m.sajadi.sut@gmail.com>
2023-11-16 20:25:09 +0330

    Lecture15 - Alphabet Distance

---
+++ Lecture/Lecture15-AlphabetDistance.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char FirstAlphabet, SecondAlphabet;
+    cin >> FirstAlphabet >> SecondAlphabet;
+    cout << int(SecondAlphabet) - int(FirstAlphabet);
+    return 0;
+}
