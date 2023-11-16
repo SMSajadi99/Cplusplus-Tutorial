3c4b551e73c62db6bbc268f970df6663acab123e
Seyed Mohammad Sajadi <s.m.sajadi.sut@gmail.com>
2023-11-16 15:20:09 +0330

    Lecture05 - Variable

---
+++ Lecture/Lecture05-Variable.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float f = 123.123;
+    int x = 45;
+    cout << "The int var is: " << x << " and the float var is: " << f << endl;
+    return 0;
+}
