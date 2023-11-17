223d5dcba8c123d3f3154231473460d58e995c75
Seyed Mohammad Sajadi <s.m.sajadi.sut@gmail.com>
2023-11-18 01:30:45 +0330

    Lecture17 - Swimming pool floor tiling

---
+++ Lecture/Lecture17-SwimmingPoolFloorTiling.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include <sstream>
+using namespace std;
+
+int main()
+{
+    string input; 
+    int single_tile = 4 * 5;
+
+    getline(std::cin, input); // Read the entire line
+
+    // Use istringstream to split the line
+    istringstream iss(input);
+
+    int firstNumber, secondNumber;
+
+    // Extract the first and second numbers from the stringstream
+    iss >> firstNumber >> secondNumber;
+
+    cout << (firstNumber * 100 * secondNumber * 100) / single_tile;
+    return 0;
+}
