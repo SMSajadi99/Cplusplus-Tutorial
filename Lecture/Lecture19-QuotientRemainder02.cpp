76840f202858ec237bdf357b52e693bbeaaf234a
Seyed Mohammad Sajadi <s.m.sajadi.sut@gmail.com>
2023-11-18 21:33:07 +0330

    Lecture19 - Quotient Remainder 02

---
+++ Lecture/Lecture19-QuotientRemainder02.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "2 / 3 = " << 2 / 3 << endl;
+    cout << "2.0 / 3 = " << 2.0 / 3 << endl;
+    cout << "2 / 3.0 = " << 2 / 3.0 << endl;
+    cout << "2.0 / 3.0 = " << 2.0 / 3.0 << endl;
+    return 0;
+}
