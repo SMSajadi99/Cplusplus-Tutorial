5812a0d00c376bc48b6b747b8c1bec7cd616569d
Seyed Mohammad Sajadi <s.m.sajadi.sut@gmail.com>
2023-11-20 21:25:27 +0330

    Lecture26 - Score

---
+++ Lecture/Lecture26-Score.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float Score;
+    cin >> Score;
+
+    if (Score > 10)
+    {
+        cout << "Pass";
+    }
+    else
+    {
+        cout << "Fail";
+    }
+    return 0;
+}
