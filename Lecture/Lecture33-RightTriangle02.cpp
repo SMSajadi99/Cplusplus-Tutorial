1dee0d83578b5ac93d91fb0a1b8c6019196eeb71
Seyed Mohammad Sajadi <s.m.sajadi.sut@gmail.com>
2023-11-23 23:48:44 +0330

    Lecture33 - Right Triangle 02

---
+++ Lecture/Lecture33-RightTriangle02.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<sstream>
+using namespace std;
+
+int main()
+{
+    string input; 
+    getline(cin, input); // Read the entire line
+
+    // Use istringstream to split the line
+    istringstream iss(input);
+
+    int angle1, angle2, angle3;
+
+    // Extract the first and second numbers from the stringstream
+    iss >> angle1 >> angle2 >> angle3;
+
+    if ((angle1 + angle2 + angle3 == 180) && (angle1 == 90 || angle2 == 90 || angle3 == 0))
+    {
+        cout << "Bale";
+    }
+    else
+    {
+        cout << "Na";
+    }
+
+    return 0;
+}
