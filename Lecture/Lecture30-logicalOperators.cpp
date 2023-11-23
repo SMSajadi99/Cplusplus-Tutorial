a0471e2361a1eafa934590065800221fed36c568
Seyed Mohammad Sajadi <s.m.sajadi.sut@gmail.com>
2023-11-23 21:27:34 +0330

    Lecture30 - logical Operators

---
+++ Lecture/Lecture30-logicalOperators.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    if (1 == 1 && 2 == 1)
+    {
+        cout << "shart 2 == 1 ghalate!" << endl;
+    }
+    else if (2 == 1 && 1 == 1)
+    {
+        cout << "inja ham shart 2 == 1 ghalate!" << endl;
+    }
+    else if (2 == 2 && 1 < 2)
+    {
+        cout << "Ahsant! in dastoor ejra mishe" << endl;
+    }
+    return 0;
+}
