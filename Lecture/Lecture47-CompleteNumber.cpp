fef62f337434d67befeffce732b07f96c28502bd
Seyed Mohammad Sajadi <s.m.sajadi.sut@gmail.com>
2024-03-25 21:12:48 +0330

    Lecture 47 Complete Number

---
+++ Lecture/Lecture47-CompleteNumber.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <sstream>
+#include <cmath>
+
+using namespace std; 
+
+int main()
+{
+    int sum_num = 0, num;
+    cin >> num;
+
+    for (int i = 1; i < num; i++)
+    {
+        if (num % i == 0)
+        {
+            sum_num = sum_num + i;
+        }
+        
+    }
+    if (sum_num == num)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+    
+    
+   
+    return 0;
+}
