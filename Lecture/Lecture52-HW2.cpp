3f0b230f7fccf6bf3b85df2d1dd776692144a354
Seyed Mohammad Sajadi <s.m.sajadi.sut@gmail.com>
2024-03-26 00:45:41 +0330

    Lecture 52 HW 2

---
+++ Lecture/Lecture52-HW2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    cin >> name;
+    cout << "aji maji la taraji " << name << "." << endl;
+    cout << "man ghol midam dige taghallob nakonam."<< endl;
+    cout << "vagarna khoda mano soosk kone!"<< endl;
+    return 0;
+}
