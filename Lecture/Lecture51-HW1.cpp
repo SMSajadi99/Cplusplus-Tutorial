493ac072d1fd238cc2eece9d82c65cd9ac4c67be
Seyed Mohammad Sajadi <s.m.sajadi.sut@gmail.com>
2024-03-26 00:45:29 +0330

    Lecture 51 HW 1

---
+++ Lecture/Lecture51-HW1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "bal e pashe " << endl;
+    cout << "sooske abi"<< endl;
+    cout << "tof e tak shakh"<< endl;
+    cout << "rishe bid e majnoon"<< endl;
+    cout << "khak e ros e sad sale"<< endl;
+    cout << "serke amme leyla"<< endl;
+    cout << "mooy e khers"<< endl;
+    return 0;
+}
