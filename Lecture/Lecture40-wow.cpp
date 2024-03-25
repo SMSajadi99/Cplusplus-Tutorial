cc79352288597e76155a744750351b825e703069
Seyed Mohammad Sajadi <s.m.sajadi.sut@gmail.com>
2024-03-25 06:21:51 +0330

    Lecture 40 Wow

---
+++ Lecture/Lecture40-wow.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    int number_O;
+    cin >> number_O;
+    cout << "W" << string(number_O,'o') << "w!";
+    return 0;
+}
