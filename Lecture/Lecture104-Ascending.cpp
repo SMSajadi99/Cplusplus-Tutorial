66095af5bccaa825bca7eb3669318aa31c5a193b
Seyed Mohammad Sajadi <s.m.sajadi.sut@gmail.com>
2024-05-13 23:48:12 +0330

    Create Lecture104-Ascending.cpp

---
+++ Lecture/Lecture104-Ascending.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void insertionSort(vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> numbers(n);
+    for (int i = 0; i < n; i++) {
+        cin >> numbers[i];
+    }
+
+    insertionSort(numbers);
+
+    for (int i = 0; i < n; i++) {
+        cout << numbers[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
