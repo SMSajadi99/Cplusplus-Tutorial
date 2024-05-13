c601f0ad67427fdba4ba46325fa2606351498e6a
Seyed Mohammad Sajadi <s.m.sajadi.sut@gmail.com>
2024-05-14 00:14:56 +0330

    Create Lecture113.cpp

---
+++ Lecture/Lecture113.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printAscendingIntervals(const vector<int>& arr, int k) {
+    int n = arr.size();
+    for (int i = 0; i <= n - k; ++i) {
+        bool isAscending = true;
+        for (int j = i; j < i + k - 1; ++j) {
+            if (arr[j] >= arr[j + 1]) {
+                isAscending = false;
+                break;
+            }
+        }
+        if (isAscending) {
+            cout << i << " to " << i + k - 1 << endl;
+        }
+    }
+}
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    printAscendingIntervals(arr, k);
+
+    return 0;
+}
